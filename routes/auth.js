79d163bb5d1da2b66dfde384cfdf148bcb98fc37
Sean Murakami <murakami.sean@yahoo.com>
2020-11-04 20:54:19 -0800

    Validates whether user already exists in db

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,14 @@ router.post("/register", async (req, res) => {
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  //Check is the user already exists
+  const emailExists = await User.findOne({
+    email: req.body.email,
+  });
+  if (emailExists)
+    return res.status(400).send("User with that email address already exists.");
+
+  //Create a new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
