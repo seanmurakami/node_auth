3827634a19f20c78978e15c9c41d728ee8bd7d2a
Sean Murakami <murakami.sean@yahoo.com>
2020-11-04 20:51:24 -0800

    Added data validation to user registration

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,14 @@
-const { response } = require("express");
-
 const router = require("express").Router();
 const User = require("../model/User");
+const { registerValidation } = require("../validation");
 
 //Post route to /api/user/register
 //async call because it takes time to post to the db
 router.post("/register", async (req, res) => {
+  //Validate data before registering user
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
